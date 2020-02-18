f7d574a8e0529500a152a83c67b81b8498145f6a
Luis S. Flores <saloch_1s@hotmail.com>
2020-02-18 16:05:46 -0600

    Feat[Security]: Add all chapter 4 specs

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -1,54 +1,100 @@
 package tacos.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig  extends WebSecurityConfigurerAdapter {
-
-
-    @Autowired
-    DataSource dataSource;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth)
-    throws Exception {
-
-        //inner private class to increase readability.
-        //SecurityConfigQueries are just queries for *THIS* file
-        SecurityConfigQueries scq = new SecurityConfigQueries();
-
-        auth
-            .jdbcAuthentication()
-            .dataSource(dataSource)
-            .usersByUsernameQuery(scq.usersQuery)
-            .authoritiesByUsernameQuery(scq.authoritiesQuery);
-
-
-
-
-
-
-
-
-
-
-    }
-
-
-    private static class SecurityConfigQueries {
-        public String usersQuery = "select username, password, enabled from Users " +
-                "where username=?";
-
-        public String authoritiesQuery = "select username, authority from UserAuthorities " +
-                "where username=?";
-    }
-
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  
+  
+  @Autowired
+  DataSource dataSource;
+  
+  @Autowired
+  private UserDetailsService userDetailsService;
+  
+  
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+        .authorizeRequests()
+        .antMatchers("/design", "/orders")
+        .hasRole("ROLE_USER")
+        .antMatchers("/", "/**").access("permitAll")
+        .and()
+        .formLogin()
+        .loginPage("/login")
+        .and()
+        .logout()
+        .logoutSuccessUrl("/");
+  }
+  
+  
+  /*@Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+        .authorizeRequests()
+        .antMatchers("/design", "/orders")
+        .access("hasRole('ROLE_USER') && " +
+            "T(java.util.Calendar).getInstance().get(" +
+            "T(java.util.Calendar).DAY_OF_WEEK) == " +
+            "T(java.util.Calendar).TUESDAY")
+        .antMatchers(“ /”,"/**").access("permitAll")
+    ;
+  }
+  * With SpEL-based security constraints, the possibilities are virtually endless.
+  * I’ll bet that you’re already dreaming up interesting
+  * security constraints based on SpEL.
+  */
+  
+  
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth)
+      throws Exception {
+    
+    auth
+        .userDetailsService(userDetailsService)
+        .passwordEncoder(encoder());
+    
+    /*auth
+        .jdbcAuthentication()
+        .dataSource(dataSource)
+        .usersByUsernameQuery(secConfQueries().usersQuery)
+        .authoritiesByUsernameQuery(secConfQueries().authoritiesQuery)
+        .passwordEncoder(passwordEncoder()); //StandardPasswordEncoder is deprecated*/
+    
+  }
+  
+  
+  //To be honest, I just want to clean the configure function... no static sounds just a clean read.
+  @Bean
+  public PasswordEncoder encoder() {
+    return new BCryptPasswordEncoder();
+  }
+  
+  @Bean
+  public SecurityConfigQueries secConfQueries() {
+    return new SecurityConfigQueries();
+  }
+  
+  private static class SecurityConfigQueries {
+    public String usersQuery = "select username, password, enabled from Users " +
+        "where username=?";
+    
+    public String authoritiesQuery = "select username, authority from UserAuthorities " +
+        "where username=?";
+  }
+  
 }
--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -2,20 +2,19 @@ package tacos.web;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.validation.Errors;
-import javax.validation.Valid;
-
-
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import tacos.data.OrderRepository;
 import tacos.model.Order;
+import tacos.model.User;
+
+import javax.validation.Valid;
 
 
 @Slf4j
@@ -23,28 +22,43 @@ import tacos.model.Order;
 @RequestMapping("/orders")
 @SessionAttributes("order")
 public class OrderController {
-
-    private OrderRepository orderRepo;
-
-    public OrderController (OrderRepository orderRepo){
-        this.orderRepo = orderRepo;
-    }
-
-
-
-    @GetMapping("/current")
-    public String orderForm() {
-        return "orderForm";
-    }
-
-    @PostMapping
-    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus) {
-        if (errors.hasErrors()) {
-            return "orderForm";
-        }
-        orderRepo.save(order);
-        sessionStatus.setComplete();
-        log.info("Order submitted: " + order);
-        return "redirect:/";
-    }
+  
+  private OrderRepository orderRepo;
+  
+  public OrderController(OrderRepository orderRepo) {
+    this.orderRepo = orderRepo;
+  }
+  
+  
+  @GetMapping("/current")
+  public String orderForm() {
+    return "orderForm";
+  }
+  
+  /*
+  Authentication authentication =
+      SecurityContextHolder.getContext().getAuthentication();
+  User user = (User) authentication.getPrincipal();
+  
+  Although this snippet is thick with security-specific code,
+  it has one advantage over the other approaches described:
+  it can be used anywhere in the application, not only in a controller’s handler methods.
+  */
+  
+  @PostMapping
+  public String processOrder(@Valid Order order, Errors errors,
+                             SessionStatus sessionStatus, @AuthenticationPrincipal User user) {
+    
+    if (errors.hasErrors()) return "orderForm";
+    
+    order.setUser(user);
+    
+    orderRepo.save(order);
+    
+    sessionStatus.setComplete();
+    
+    
+    log.info("Order submitted: " + order);
+    return "redirect:/";
+  }
 }
